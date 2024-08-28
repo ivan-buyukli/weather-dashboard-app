9556b727c1ccaf752024bf67917d20ef6c9fa7ce
Ivan Buyukli <ivan.buyukli21@gmail.com>
2024-08-28 21:29:34 +0200

    initial page before splitting to smaller components

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {fetchWeatherApi} from "openmeteo";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'weather-dashboard-app';
+  params = {
+    latitude: [52.54],
+    longitude: [13.41],
+    current: 'temperature_2m,weather_code,wind_speed_10m,wind_direction_10m',
+    hourly: 'temperature_2m,precipitation',
+    daily: 'weather_code,temperature_2m_max,temperature_2m_min'
+  };
+  url = 'https://api.open-meteo.com/v1/forecast';
+
+  async fetchWeatherApi() {
+    return await fetchWeatherApi(this.url, this.params);
+  }
+
+  ngOnInit(): void {
+    this.fetchWeatherApi().then(response => console.log(response[0].daily()?.variables(1)?.valuesArray()))
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,14 +2,22 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import {NgOptimizedImage} from "@angular/common";
+import {HttpClientModule} from "@angular/common/http";
+import { CityComponent } from './city/city.component';
+import { WeatherComponent } from './weather/weather.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CityComponent,
+    WeatherComponent
   ],
-  imports: [
-    BrowserModule
-  ],
+    imports: [
+        BrowserModule,
+        NgOptimizedImage,
+        HttpClientModule
+    ],
   providers: [],
   bootstrap: [AppComponent]
 })
